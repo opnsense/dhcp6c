8d92b2c045dc0a4eeebfdf5ecb11f373bfe6b58d
SUZUKI Shinsuke <suz@kame.net>
2006-09-02 08:19:11 +0000

    fixed a compilation failure in freebsd4

--- lease.c
+++ lease.c
@@ -33,7 +33,16 @@
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/queue.h>
-#include <sys/time.h>
+#if TIME_WITH_SYS_TIME
+# include <sys/time.h>
+# include <time.h>
+#else
+# if HAVE_SYS_TIME_H
+#  include <sys/time.h>
+# else
+#  include <time.h>
+# endif
+#endif
 #include <netinet/in.h>
 #include "dhcp6.h"
 #include "config.h"
@@ -46,7 +55,7 @@
 
 struct hash_entry {
 	LIST_ENTRY(hash_entry) list;
-	char val[];
+	char *val;
 };
 
 LIST_HEAD(hash_head, hash_entry);
