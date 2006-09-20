04867277df66009142b9346abfe0276b7a66f857
SUZUKI Shinsuke <suz@kame.net>
2006-09-20 08:22:24 +0000

    fixed a bug that status-code option might include a unexpected garbage value.  (Bug-ID 1561202)

--- common.c
+++ common.c
@@ -2166,7 +2166,7 @@ dhcp6_set_options(type, optbp, optep, optinfo)
 	     stcode = TAILQ_NEXT(stcode, link)) {
 		u_int16_t code;
 
-		code = htons(stcode->val_num);
+		code = htons(stcode->val_num16);
 		if (copy_option(DH6OPT_STATUS_CODE, sizeof(code), &code, &p,
 		    optep, &len) != 0) {
 			goto fail;
