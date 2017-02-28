4fdb7b2be4c7e940f43d5726e752199738eafe91
Hiroki Sato <hrs@allbsd.org>
2017-03-01 03:04:51 +0900

    Use LOG_INFO to log an address change

--- common.c
+++ common.c
@@ -3346,7 +3346,7 @@ ifaddrconf(cmd, ifname, addr, plen, pltime, vltime)
 	}
 #endif
 
-	d_printf(LOG_DEBUG, FNAME, "%s an address %s/%d on %s", cmdstr,
+	d_printf(LOG_INFO, FNAME, "%s an address %s/%d on %s", cmdstr,
 	    addr2str((struct sockaddr *)addr), plen, ifname);
 
 	close(s);
