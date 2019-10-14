10f3f7f90d29b27a9d7f9b60f885595c1ff5d377
Martin Wasley <martin@queens-park.com>
2019-10-14 23:03:28 +0100

    Log Prefix Info
    Change the logging level to show prefix information in INFO logs rather than DEBUG logs
    
    Added - Using the 'D' option for extended debug will now force the debug to output as DEBUG_INFO.
    This means no changes are required to the syslog within Opnsense itself.

--- prefixconf.c
+++ prefixconf.c
@@ -192,7 +192,7 @@ update_prefix(ia, pinfo, pifc, dhcpifp, ctlp, callback)
 	/* update the prefix according to pinfo */
 	sp->prefix.pltime = pinfo->pltime;
 	sp->prefix.vltime = pinfo->vltime;
-	d_printf(LOG_DEBUG, FNAME, "%s a prefix %s/%d pltime=%lu, vltime=%lu",
+	d_printf(LOG_INFO, FNAME, "%s a prefix %s/%d pltime=%lu, vltime=%lu",
 	    spcreate ? "create" : "update",
 	    in6addr2str(&pinfo->addr, 0), pinfo->plen,
 	    pinfo->pltime, pinfo->vltime);
