90b774d053b6aaf55ae5f657cba138f057c23d58
Franco Fichtner <franco@opnsense.org>
2024-06-06 07:30:55 +0200

    config: shorten the code
    Multiple ifid statements will be accepted.  The last one wins.
    This is the same as ifid-random, either one of the last one wins.
    
    To be honest I'd rather have liked to see
    
        ifid random;
    
    This is a bit more intuitive from the user side and allows to
    make the override of the override to make more sense as it is
    still the same statement.

--- config.c
+++ config.c
@@ -582,19 +582,12 @@ add_pd_pif(iapdc, cfl0, if_count)
 				goto bad;
 			}
 			break;
-		case IFPARAM_IFID:
-			if (use_default_ifid) {
-				for (i = sizeof(pif->ifid) - 1; i >= 0; i--) {
-					pif->ifid[i] = (cfl->num >> 8 *
-					    (sizeof(pif->ifid) - 1 - i)) & 0xff;
-				}
-				use_default_ifid = 0;
-			}
-			break;
 		case IFPARAM_IFID_RAND:
 			for (i = 0; i < pif->ifid_len ; i++) {
 				cfl->num = cfl->num * 2 + rand() % 2;
 			}
+			/* FALLTRHROUGH */
+		case IFPARAM_IFID:
 			for (i = sizeof(pif->ifid) -1; i >= 0; i--) {
 				pif->ifid[i] = (cfl->num >> 8 *
 				    (sizeof(pif->ifid) - 1 - i)) & 0xff;
